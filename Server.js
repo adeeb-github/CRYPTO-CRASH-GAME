3d8531dc3d1482a9e9cb23b953ef0ce512e7be02
adeeb-github <112182422+adeeb-github@users.noreply.github.com>
2025-07-25 23:12:56 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
-const { Server } = require("socket.io");
+const { Server } = require("Socket.io");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
